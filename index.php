1cf052aee9869699fabc0123e76b01207d48635c
rugby-streamer <sohil.business@gmail.com>
2017-02-16 20:54:51 +1300

    index.php
    index.php new file

---
+++ index.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://ogp.me/ns/fb#" xml:lang="en, de" lang="en" dir="ltr">
+<head>
+<title>Rugby Live Streaming | Rugby Live Stream</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="description" content="Rugby Live Streaming  | Rugby Live Stream | Enjoy high quality Rugby streaming in HD only | Working on any devices | Click and Play at No Costs  | mobile vision view">
+<meta name="keywords" content="live, stream, sport, television, tv, iptv,Rugby, SportsRugby, Rugby union, World Rugby, IRB, Sevens, world rankings, fixtures, results, member unions, regional associations, laws of the game, regulations, development, training, player welfare, women’s rugby">
+
+<meta name="author" content="Wespeakrugby">
+<meta name=copyright content="Rugby Live Streaming | Rugby Live Stream">
+<meta name=Charset content=ISO-8859-1>
+<meta name=Distribution content=Global>
+<meta name=Rating content=General>
+<meta name=Robots content="INDEX,FOLLOW">
+<meta name=Revisit-after content="1 Day">
+<meta http-equiv="content-language" content="en-us"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="/images/favnew.ico" rel="shortcut icon" type="image/vnd.microsoft.icon"/>
+
+
+<meta property="og:title" content=" Watch Brisbane 10s Live Stream In HD" />
+<meta property="og:type" content="article" />
+<meta property="og:url" content="http://brisbane10s.is-best.net" />
+<meta property="og:image" content="http://brisbane10s.is-best.net/Brisbane-Global.png" />
+<meta property="og:image:width" content="500" />
+<meta property="og:image:height" content="267" />
+<meta property="og:description" content="Please share the stream & enjoy" />
+
+
+<script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript" id="st_insights_js" src="http://w.sharethis.com/button/buttons.js?publisher=c0522b29-5a85-4a9c-a9c7-330dc4d5cb13"></script>
+<script type="text/javascript">stLight.options({publisher: "c0522b29-5a85-4a9c-a9c7-330dc4d5cb13", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+</head>
+<body style="background-color: #000000;">
+<!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.startgif', '1,3719404,4,10003,"div#histatsC {position: absolute;top:0px;left:0px;}body>div#histatsC {position: fixed;}"']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_giftop_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><style type="text/css">div#histatsC {position: absolute;top:0px;left:0px;}body>div#histatsC {position: fixed;}</style>
+<a href="/" alt="frontpage hit counter" target="_blank" ><div id="histatsC"><img border="0" src="http://s4is.histats.com/stats/i/3719404.gif?3719404&103"></div></a>
+</noscript>
+        <!-- Histats.com  END  -->
+<IFRAME FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=800 HEIGHT=500 SRC="http://2-live-wespeakrugby.is-great.net/" ALLOWFULLSCREEN></IFRAME>
+<div
+<div>
+<a href="https:&#x2F;&#x2F;www.namecheap.com&#x2F;?aff=110452"><img src="http:&#x2F;&#x2F;files.namecheap.com&#x2F;graphics&#x2F;linkus&#x2F;468x60-1.gif" width="468" height="60" border="0" alt="Namecheap.com"></a><!-- END LINK --><div style="text-align:center">
+<script id="cid0020000146882431628" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 270px;height: 300px;">{"handle":"wespeakrugby-talk","arch":"js","styles":{"a":"000000","b":100,"c":"FFFFFF","d":"FFFFFF","k":"000000","l":"000000","m":"000000","n":"FFFFFF","p":"10","q":"000000","r":100,"pos":"br","cv":1,"cvbg":"000000","cvw":270,"cvh":44,"ticker":1,"fwtickm":1}}</script>
+<div style="text-align:center">
+<iframe src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fbrisbane10s.is-best.net&layout=button_count&size=small&mobile_iframe=true&appId=694218780737861&width=68&height=20" width="68" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+<span class='st_twitter_hcount' displayText='Tweet'></span>
+<span class='st_email_hcount' displayText='Email'></span>
+</body>
+</noscript>
+<div style="text-align: center;"><div style="position:relative; top:0; margin-right:auto;margin-left:auto; z-index:99999">
+
+</div></div>
+</html>
